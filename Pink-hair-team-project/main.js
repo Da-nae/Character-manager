2af3791262aeb09c67bc0255dcf6c938cf09d810
Da-nae <grosjean.danae@gmail.com>
2022-11-09 09:19:08 +0100

    axios test

--- Pink-hair-team-project/main.js
+++ Pink-hair-team-project/main.js
@@ -1,4 +1,8 @@
 import './style.css'
+const axios = import('axios').default;
 
 
-
+axios.get('https://static.becode.xyz/character-database/characters-database.postman_collection.json')
+  .then(response => {
+    console.log(response.data);
+  })
