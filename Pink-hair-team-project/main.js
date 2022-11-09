9eb0b266b972d99efab476d77b6a545ea24a9247
Da-nae <grosjean.danae@gmail.com>
2022-11-09 15:53:00 +0100

    await modified

--- Pink-hair-team-project/main.js
+++ Pink-hair-team-project/main.js
@@ -1,5 +1,4 @@
 // Import files :
-
 import './style.css'
 import axios from 'axios'
 
@@ -20,28 +19,29 @@ const AllCards = async() => {
   let AllCards = await getAllCharacterAsync();
 }
 
+
 /** Fetch one of the characters in the database by its ID
 * @author Danaé Grosjean
 * @param {string} value - NEED TO BE CHANGED - an ID to test the function
 * @param {object} characterById - host the response of the get demand on the data base
 * @returns {object} return the card of a character already in the database
+*
+*
+async function getCharacterByIdAsync() {
+  let value = "301bc33c-62fe-4060-8047-45ca196019da";
+  const response = await axios.get(`https://character-database.becode.xyz/characters/${value}`);
+
+  let characterById = response.data;
+  return characterById;
+}
 */
-
-  async function getCharacterByIdAsync() {
-    let value = "301bc33c-62fe-4060-8047-45ca196019da";
-    const response = await axios.get(`https://character-database.becode.xyz/characters/${value}`);
-
-    let characterById = response.data;
-    return characterById;
-  }
 
 /** Fetch one of the characters in the database by its name
 * @author Danaé Grosjean
 * @param {string} value - NEED TO BE CHANGED - a name to test the function
 * @param {array} characterByName - host the response of the get demand on the data base
 * @returns {array} return the card of a character already in the database
-*/
-
+*
 async function getCharacterByName() {
   let value = "Spiderman";
   const response = await axios.get(`https://character-database.becode.xyz/characters?name=${value}`);
@@ -49,6 +49,8 @@ async function getCharacterByName() {
   let characterByName = response.data;
   return characterByName;
 }
+*/
+
 
 /** Display all characters in the main html file
 * @author Danaé Grosjean
@@ -59,25 +61,31 @@ async function getCharacterByName() {
 function displayAllCharacters () {
 
   for (let elem of AllCards) {
+
+// Creation of the div to host the card
     let card = document.createElement('div');
     document.body.appendChild(card);
     card.classList.add("character");
 
+// Adding the image of the character
     let pic = document.createElement('img');
     card.appendChild(pic);
     pic.src = `data:image/gif;base64,${elem.image}`;
     pic.classList.add("character_img");
 
+// Adding the name of the character
     let title = document.createElement('h2');
     title.textContent = elem.name;
     card.appendChild(title);
     title.classList.add("character_name");
 
+// Adding the small description of the character
     let infoShort = document.createElement('p');
     card.appendChild(infoShort);
     infoShort.textContent = elem.shortDescription;
     infoShort.classList.add("character_infos");
 
+// Adding the button more info on the character
     let infoButton = document.createElement('a');
     card.appendChild(infoButton);
     infoButton.textContent = "More info";
@@ -87,5 +95,3 @@ function displayAllCharacters () {
 }
 
 displayAllCharacters();
-
-
