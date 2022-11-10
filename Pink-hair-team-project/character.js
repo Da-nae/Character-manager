ff659fe3600afa99f94d348ab0dcb87e8b2dfefe
Da-nae <grosjean.danae@gmail.com>
2022-11-10 10:06:27 +0100

    add delete button, fixed await on character page

--- Pink-hair-team-project/character.js
+++ Pink-hair-team-project/character.js
@@ -1,47 +1,49 @@
 // Import files :
-
+import './style.css'
 import axios from 'axios'
 
-/** Fetch one of the characters in the database by its name
-* @author Danaé Grosjean
-* @param {string} value - NEED TO BE CHANGED - a name to test the function
-* @param {array} characterByName - host the response of the get demand on the data base
-* @returns {array} return the card of a character already in the database
-*/
-
-async function getCharacterByIdAsync() {
-    let value = "301bc33c-62fe-4060-8047-45ca196019da";
-    const response = await axios.get(`https://character-database.becode.xyz/characters/${value}`);
-
-    let characterById = response.data;
-    return characterById;
-}
-
-let singleCharacter = await getCharacterByIdAsync();
-console.log(singleCharacter);
-
-/** Display a single characters in the main html file
+/** Fetch one of the characters in the database by its ID and display it in a single page
 * @author Danaé Grosjean
 * @param {string} value - NEED TO BE CHANGED - a name to test the function
 * @returns {array} return in the DOM all the name, image and description of all the database characters, each in a different div
 */
 
-function displayCharacterAsync () {
+async function displayCharacterAsync() {
 
+    let value = "bb9afad9-2120-4fba-9929-e450f1385cb1";
+    const response = await axios.get(`https://character-database.becode.xyz/characters/${value}`);
+
+    let singleCharacter = response.data;
+
+// Creation of the div to host the card
         let card = document.createElement('div');
         document.body.appendChild(card);
-    
+        card.classList.add("character");
+
+// Adding the image of the character
         let pic = document.createElement('img');
         card.appendChild(pic);
         pic.src = `data:image/gif;base64,${singleCharacter.image}`;
-    
+        pic.classList.add("character_img");
+
+// Adding the name of the character
         let title = document.createElement('h2');
         title.textContent = singleCharacter.name;
         card.appendChild(title);
-    
+        title.classList.add("character_name");
+
+// Adding the small description of the character
         let infoLong = document.createElement('p');
         card.appendChild(infoLong);
         infoLong.textContent = singleCharacter.description;
+        infoLong.classList.add("character_description");
+
+// Adding the button more info on the character
+        let infoButton = document.createElement('a');
+        card.appendChild(infoButton);
+        infoButton.textContent = "More info";
+        infoButton.href = `https://character-database.becode.xyz/characters?name=${singleCharacter.name}`;
+        infoButton.classList.add("character_buttons");
 
 }
 
