ae587fcaed1755c21ca88fcf7d4f0adb098bc5fc
Da-nae <grosjean.danae@gmail.com>
2022-11-14 10:46:59 +0100

    adding the input character

--- Pink-hair-team-project/input.js
+++ Pink-hair-team-project/input.js
@@ -2,15 +2,50 @@
 import './style.css'
 import axios from 'axios'
 
-let characterName = document.getElementById('chatacterName');
-let image = document.getElementById('image');
-let shortDescription = document.getElementById('shortDescription');
+// Submit button : 
+let submit = document.getElementById('submit');
 
+// read the image file : 
+let image = document.querySelector("input[type=file]").addEventListener("change", (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onloadend = () => {
+        image = reader.result.replace('data:', '').replace(/^.+,/, '');
+    };
+    reader.readAsDataURL(file)
+});
+
+// function to add the data in the database : 
 async function inputCharacterAsync() {
-    const response = await axios.post('https://character-database.becode.xyz/characters');
 
-    const AllCharacters = response.data;
-    return AllCharacters;
+    let characterName = document.querySelector('#characterName').value;
+    let shortDescription = document.querySelector('#shortDescription').value;
+    let Description = document.querySelector('#Description').value;
+    
+        let newChar = { 
+            name: characterName, 
+            shortDescription: shortDescription,
+            description: Description,
+            image: image
+        };
+    
+        let response = await axios.post('https://character-database.becode.xyz/characters', newChar);
+    
+        let data = response.data;
+        console.log(data);
 }
 
-inputCharacterAsync();
+// Function to submit the data : 
+submit.addEventListener("click", async() => {
+
+    const result = confirm("Is your character ready to be added to the database?");
+
+        if (result) {
+            if(characterName.length == 0 || shortDescription.length == 0 || Description.length == 0 || document.querySelector("input[type=file]").files.length == 0) {
+                alert("You let something empty... try again !")
+            } else {
+                alert("Congrats ! It's added !");
+                await inputCharacterAsync();
+            }
+        }
+});
