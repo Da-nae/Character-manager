f8c2a9be393ecfdd53aa03d91e2340726b121475
Da-nae <grosjean.danae@gmail.com>
2022-11-15 14:00:11 +0100

    update css

--- Pink-hair-team-project/main.js
+++ Pink-hair-team-project/main.js
@@ -2,54 +2,6 @@
 import './style.css'
 import axios from 'axios'
 
-/** Fetch all the characters in the database
-* @author Danaé Grosjean
-* @param {array} AllCharacters - host the response of the get demand on the data base
-* @returns {array} AllCharacters - an array filled with the entries of the database
-*/
-// async function getAllCharacterAsync() {
-//     const response = await axios.get('https://character-database.becode.xyz/characters');
-
-//     const AllCharacters = response.data;
-//     return AllCharacters;
-// }
-
-// const AllCards = async() => {
-//   return await getAllCharacterAsync();
-// }
-
-/** Fetch one of the characters in the database by its ID
-* @author Danaé Grosjean
-* @param {string} value - NEED TO BE CHANGED - an ID to test the function
-* @param {object} characterById - host the response of the get demand on the data base
-* @returns {object} return the card of a character already in the database
-*
-*
-async function getCharacterByIdAsync() {
-  let value = "301bc33c-62fe-4060-8047-45ca196019da";
-  const response = await axios.get(`https://character-database.becode.xyz/characters/${value}`);
-
-  let characterById = response.data;
-  return characterById;
-}
-*/
-
-/** Fetch one of the characters in the database by its name
-* @author Danaé Grosjean
-* @param {string} value - NEED TO BE CHANGED - a name to test the function
-* @param {array} characterByName - host the response of the get demand on the data base
-* @returns {array} return the card of a character already in the database
-*
-async function getCharacterByName() {
-  let value = "Spiderman";
-  const response = await axios.get(`https://character-database.becode.xyz/characters?name=${value}`);
-
-  let characterByName = response.data;
-  return characterByName;
-}
-*/
-
-
 /** Display all characters in the main html file
 * @author Danaé Grosjean
 * @returns {array} return in the DOM all the name, image and description of all the database characters, each in a different div
